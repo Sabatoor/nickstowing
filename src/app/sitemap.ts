7f87da7c7df3ebdd44212dd864567fdc525f33ee
Neil Mastroianni <nf_mastroianni@hotmail.com>
2024-03-23 18:57:40 -0400

    added sitemap and robots

---
+++ src/app/sitemap.ts
@@ -0,0 +1,33 @@
+import { createClient } from '@/prismicio'
+import { asDate } from '@prismicio/client'
+export default async function sitemap() {
+  const client = createClient()
+  const settings = await client.getSingle('settings')
+  const pages = await client.getAllByType('page')
+  const sitemapPages = pages.map(page => ({
+    url: `https://${settings.data.domain || `example.com`}${page.url}`,
+    lastModified: asDate(page.last_publication_date),
+  }))
+  const homepage = await client.getSingle('homepage')
+  const sitemapHomepage = {
+    url: `https://${settings.data.domain || `example.com`}${homepage.url}`,
+    lastModified: asDate(homepage.last_publication_date),
+  }
+  const services = await client.getAllByType('service')
+  const sitemapPosts = services.map(service => ({
+    url: `https://${settings.data.domain || `example.com`}${service.url}`,
+    lastModified: asDate(service.last_publication_date),
+  }))
+  const portfolios = await client.getAllByType('portfolio')
+  const sitemapPortfolios = portfolios.map(portfolio => ({
+    url: `https://${settings.data.domain || `example.com`}${portfolio.url}`,
+    lastModified: asDate(portfolio.last_publication_date),
+  }))
+
+  return [
+    sitemapHomepage,
+    ...sitemapPages,
+    ...sitemapPosts,
+    ...sitemapPortfolios,
+  ]
+}
